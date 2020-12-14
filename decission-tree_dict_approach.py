e038ed8e7dff433114fc954071d454abf2f7a9ae
Rohith <rohithkalluru@gmail.com>
2020-12-14 10:23:00 +0530

    working decision tree dictionary

--- decission-tree_dict_approach.py
+++ decission-tree_dict_approach.py
@@ -6,119 +6,213 @@ import pprint
 import pandas as pd
 import sys
 sys.setrecursionlimit(1500)
+class decision_tree :
+    def entropy(self,col,name):
+        tot = col.shape[0]
+        uni = col[name].value_counts().rename_axis('unique').reset_index(name='counts')
+        uni['p'] = uni['counts']/tot
+        # uni['inf'] = uni['p']*(uni['p'].apply(lambda x: x*math.log2(x)))
+        uni['p'] = uni['p'].apply(lambda x: -1*(x*math.log2(x)))
+        # print(col.shape[0])
+        # print(uni)
 
-def entropy(col,name):
-    tot = col.shape[0]
-    uni = col[name].value_counts().rename_axis('unique').reset_index(name='counts')
-    uni['p'] = uni['counts']/tot
-    # uni['inf'] = uni['p']*(uni['p'].apply(lambda x: x*math.log2(x)))
-    uni['p'] = uni['p'].apply(lambda x: -1*(x*math.log2(x)))
-    # print(col.shape[0])
-    # print(uni)
+        return uni['p'].sum()
 
-    return uni['p'].sum()
+    def gain(self,x,y,features):
+        # x.columns = range(x.shape[1])
+        x['out'] = y
+        x['ent/out'] = self.entropy(x,'out')
+        target_values = x['out'].value_counts().to_dict()
+        initial_size = x.shape[0]
+        for f in features:
+            # print(f)
+            values = x[f].value_counts().to_dict()
+            ent_x = 0
+            split_info = 0
+            for k1 in values.keys():
+                current_size = x[x[f] == k1].shape[0]
+                ent1 = 0
 
-def gain(x,y,features):
-    # x.columns = range(x.shape[1])
-    x['out'] = y
-    x['ent/out'] = entropy(x,'out')
-    target_values = x['out'].value_counts().to_dict()
-    initial_size = x.shape[0]
-    for f in features:
-        # print(f)
-        values = x[f].value_counts().to_dict()
-        ent_x = 0
-        split_info = 0
-        for k1 in values.keys():
-            current_size = x[x[f] == k1].shape[0]
-            ent1 = 0
+                for k2 in target_values.keys():
+                    df1 = x[(x[f] == k1)&(x['out']==k2)].shape[0]
+                    # print(k1,k2,df1,current_size,df1/current_size)
+                    if df1 != 0:
+                        ent1 += (-df1/current_size)*math.log2(df1/current_size)
 
-            for k2 in target_values.keys():
-                df1 = x[(x[f] == k1)&(x['out']==k2)].shape[0]
-                # print(k1,k2,df1,current_size,df1/current_size)
-                if df1 != 0:
-                    ent1 += (-df1/current_size)*math.log2(df1/current_size)
+                # print(current_size/initial_size)
+                ent_x += (current_size/initial_size)*ent1
+                split_info += (-current_size/initial_size)*math.log2(current_size/initial_size)
+            x['ent/'+f] = ent_x
+            x["inf/"+f] = x['ent/out']-ent_x
+            x['gain/'+f] = x['inf/'+f]/split_info
+            # print(x['gain/'+f])
 
-            # print(current_size/initial_size)
-            ent_x += (current_size/initial_size)*ent1
-            split_info += (-current_size/initial_size)*math.log2(current_size/initial_size)
-        x['ent/'+f] = ent_x
-        x["inf/"+f] = x['ent/out']-ent_x
-        x['gain/'+f] = x['inf/'+f]/split_info
-        # print(x['gain/'+f])
+        return x,self.find_winner(x)
 
-    return x,find_winner(x)
+    def find_winner(self,df):
+        features = df.columns.values
+        df_details = df[features[5:]]
+        df_details_cols = df_details.columns.values
 
-def find_winner(df):
-    features = df.columns.values
-    df_details = df[features[5:]]
-    df_details_cols = df_details.columns.values
+        best_f = {}
 
-    best_f = {}
+        for f in df_details_cols:
+            if f.find("gain/") != -1:
+                best_f[f.replace("gain/","")] = df[f].unique()[0]
 
-    for f in df_details_cols:
-        if f.find("gain/") != -1:
-            best_f[f.replace("gain/","")] = df[f].unique()[0]
+        sorted_dict = list(dict(sorted(best_f.items(),key=lambda item: item[1])).keys())
 
-    sorted_dict = list(dict(sorted(best_f.items(),key=lambda item: item[1])).keys())
-
-    return sorted_dict[0]
+        return sorted_dict[0]
 
 
-def get_subtable(df, node, value):
-    '''
-    Function to get a subtable of met conditions.
+    def get_subtable(self,df, node, value):
+        '''
+        Function to get a subtable of met conditions.
 
-    node: Column name
-    value: Unique value of the column
-    '''
-    return df[df[node] == value]
+        node: Column name
+        value: Unique value of the column
+        '''
+        return df[df[node] == value]
 
-def buildTree(df,out,cols,tree={}):
+    def dict_print(self,dty):
+        print()
+        level = dty.get('level')
+        print('level :',level)
+        count = dty.get('count')
+        for k in count.keys():
+            print('count of',k,":",count.get(k))
+        print("Entropy :",round(dty.get('entropy'),2))
+        print('feature :',dty.get('feature'))
+        gain = dty.get('gain')
+        if gain != None:
+            print('gain :',round(dty.get('gain'),2))
 
-    df,node = gain(df,out,cols)
-    # print(node)
+    def buildTree(self,df,out,cols,l=0,printer=False,tree={}):
 
-    attValue = np.unique(df[node])
+        if len(cols) == 0:
+            tree['leaf'] = True
+            tree['feature'] = "Reached leaf node"
+            tree['level'] = l
+            tree['entropy'] = 0.0
+            df['out'] = out
+            tree['count'] = df['out'].value_counts().to_dict()
+            uni = df['out'].unique()
 
-    if tree == {}:
-        tree[node] = {'gain':df['gain/'+node].unique()[0],'feature':node,'entropy':df['gain/'+node].unique()[0],'count':df['out'].value_counts().to_dict()}
-        for f in df[node].unique():
-            cols = set(cols).difference(set([node]))
-            if len(cols) == 0:
-                tree['leaf'] = True
-                return
-            elif len(df['out'].unique()) == 1:
-                tree['leaf'] = True
-                return
+            if len(uni)==1:
+                tree['out'] = uni[0]
             else:
+                tree['out'] = list(uni)
+
+            if printer:
+                self.dict_print(tree)
+
+            return
+
+        df,node = self.gain(df,out,cols)
+        # print(node)
+
+
+        if len(df['out'].unique()) == 1:
+            tree['leaf'] = True
+            tree['feature'] = "Reached leaf node"
+            tree['level'] = l
+            tree['entropy'] = 0.0
+            tree['out'] = df['out'].unique()[0]
+            tree['count'] = df['out'].value_counts().to_dict()
+
+            if printer:
+                self.dict_print(tree)
+
+            return
+
+
+        if tree == {}:
+            tree[node] = {'gain':df['gain/'+node].unique()[0],'feature':node,'entropy':df['ent/'+node].unique()[0],'level':l,'count':df['out'].value_counts().to_dict(),'leaf':False}
+            tree['leaf'] = False
+            if printer:
+                self.dict_print(tree[node])
+
+            cols = set(cols).difference(set([node]))
+            l += 1
+            for f in df[node].unique():
                 tree[node][f] = {}
-                subtable = get_subtable(df, node, f)
+                subtable = self.get_subtable(df, node, f)
                 columns = subtable.columns.values
-                buildTree(subtable[columns[:4]],subtable[columns[4]],list(cols),tree[node][f])
 
-    # for value in attValue:
-    #
-    #     subtable = get_subtable(df, node, value)
-    #     clValue, counts = np.unique(subtable['out'], return_counts=True)
-    #
-    #     if len(counts) == 1:
-    #         tree[node][value] = clValue[0]
-    #     else:
-    #         features = subtable.columns.values[:5]
-    #         # print(features)
-    #         table = subtable[features[:5]]
-    #         # print(table[features[:4]])
-    #         # print(node)
-    #         # print(set(features).difference(set([node])))
-    #         tree[node][value] = buildTree(table[features[:4]],table[features[4]],[f for f in features if f != node],tree)  # Calling the function recursively
+                if printer:
+                    self.buildTree(subtable[columns[:4]], subtable[columns[4]], list(cols), l=l, tree=tree[node][f],printer=True)
+                else:
+                    self.buildTree(subtable[columns[:4]],subtable[columns[4]],list(cols),l=l,tree=tree[node][f])
 
-    return tree
+
+        return tree
+
+    def fit(self,df,out,printer=False):
+
+        columns = [str(f) for f in df.columns.values]
+        self.columns = columns
+        df.columns = columns
+
+        if printer:
+            self.root = self.buildTree(df, out, columns,printer=True)
+        else:
+            self.root = self.buildTree(df, out, columns)
+
+        return self.root
+    
+    def pretty_print(self):
+        pprint.pprint(self.root)
+        
+    def predict(self,dty):
+
+        nroot = self.root.copy()
+
+        cols = [str(i) for i in dty.keys()]
+        present = True
+        feature = ""
+        for c in cols:
+            if c not in self.columns:
+                feature = c
+                present = False
+                break
+
+        # pprint.pprint(nroot)
+
+
+        if present:
+            while nroot.get('leaf') == False:
+                key = set(list(nroot.keys())).intersection(set(cols))
+                key = list(key)[0]
+                # print(list(key)[0])
+                nroot = nroot.get(key)
+                value = dty.get(key)
+
+                if nroot.get(value) != None:
+                    nroot = nroot.get(value)
+                else:
+                    print("the value",value,"is not present in the feature",key ,"for the given combination")
+                    return
+        else:
+            print("the",feature,"is not present in the decission tree")
+
+        return nroot.get('out')
+
+
+
+
+
+
+
+
+
 
 iris_data = datasets.load_iris()
-data = pd.DataFrame(iris_data.data, columns=["sl", "sw", 'pl', 'pw'])
-target = pd.DataFrame(iris_data.target)
-# target = target.astype('bool')
+target_names = list(iris_data.target_names)
+cols = ["sepal_length", "sepal_width", 'petal_length', 'petal_width']
+data = pd.DataFrame(iris_data.data, columns=cols)
+# print(data)
+target = pd.DataFrame(iris_data.target)[0].apply(lambda x: target_names[x]).to_frame()
+
 
 def label(val, *boundaries):
     if (val < boundaries[0]):
@@ -139,17 +233,22 @@ def toLabel(df, old_feature_name):
     return df[old_feature_name].apply(label, args= (first, second, third))
 
 # print(data)
+# print(target)
 # print(iris_data.target_names)
 
-data['sl_labeled'] = toLabel(data, 'sl')
-data['sw_labeled'] = toLabel(data, 'sw')
-data['pl_labeled'] = toLabel(data, 'pl')
-data['pw_labeled'] = toLabel(data, 'pw')
+data['sl_labeled'] = toLabel(data, 'sepal_length')
+data['sw_labeled'] = toLabel(data, 'sepal_width')
+data['pl_labeled'] = toLabel(data, 'petal_length')
+data['pw_labeled'] = toLabel(data, 'petal_width')
 
-data.drop(['sl', 'sw', 'pl', 'pw'], axis = 1, inplace = True)
+data.drop(cols, axis = 1, inplace = True)
+data.columns = cols
 
 columns = data.columns.values
-target = toLabel(target,0)
-
-tree = buildTree(data,target,columns)
-pprint.pprint(tree)
+dtree = decision_tree()
+# tree = dtree.buildTree(data,target,columns,printer=True)
+tree = dtree.fit(data.copy(),target)
+# pprint.pprint(tree)
+d = {'sepal_length':'c','sepal_width':'c','petal_length':'c','petal_width':'c'}
+pred = dtree.predict(d)
+print(pred)
